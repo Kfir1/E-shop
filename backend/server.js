37834d09f19eda966640994baa89be385ef5aab8
Kfir1 <kfirrp@gmail.com>
2023-06-24 15:38:20 +0300

    add product routes(get products from DB) + add asyncHandler+ modified server.js

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ import express from 'express';
 // need to change type in root package.json (type: commonjs)
 import dotenv from 'dotenv';
 dotenv.config(); // needs to be below (import dotenv from 'dotenv';)
-import products from './data/products.js';
-// since ES modules on backend, have to use 
-// (products.js) and just products for my own javascript modules
+import productRoutes from './routes/productRoutes.js'
 import connectDB from './config/db.js';
 
 
@@ -28,19 +26,8 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-// create route to see data from products.js json file
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-// create a route for a single product
-// iterate over the prodcuts array of objects
-// if the product _id matches the product in the URL,
-// then return that product (res.json(products))
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+// every time this route is running '/api/products', will use (productRoutes) file
+app.use('/api/products', productRoutes);
 
 // start the server
 app.listen(port, () => console.log(`Server running on port ${port}`));
