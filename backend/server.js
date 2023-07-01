770d086478e9ecf5ef6929a01f4221764eda8d7c
Kfir1 <kfirrp@gmail.com>
2023-07-01 15:03:38 +0300

    added user routes and user controller, import userRoutes on server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import express from 'express';
 // need to change type in root package.json (type: commonjs)
 import dotenv from 'dotenv';
 dotenv.config(); // needs to be below (import dotenv from 'dotenv';)
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
@@ -29,6 +30,7 @@ app.get('/', (req, res) => {
 
 // every time this route is running '/api/products', will use (productRoutes) file
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
