82220a7861f0e2458ed0ad9de1d98fa9c98ae249
Kfir1 <kfirrp@gmail.com>
2023-07-19 14:07:20 +0300

    added CheckoutSteps component

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 const PrivateRoute = () => {
   const { userInfo } = useSelector(state => state.auth);
 
+  // if  exist go to 
   return (
     userInfo ? <Outlet /> : <Navigate to='/login' replace/> // replace for replacing any past history
   )
