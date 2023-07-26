080d4e83b66693f243747f3f7189605098217752
Kfir1 <kfirrp@gmail.com>
2023-07-26 18:55:37 +0300

    added admin route component + admin route in index.js + Header dropdown menu for admin

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -2,16 +2,16 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 
-const PrivateRoute = () => {
+const AdminRoute = () => {
   const { userInfo } = useSelector(state => state.auth);
 
   // if logged in, Outlet will route if not Navigate will route
   // if  exist go to 
-  return userInfo ? (
+  return userInfo && userInfo.isAdmin ? (
       <Outlet /> 
   ) : ( 
       <Navigate to='/login' replace/> // replace for replacing any past history
   );
 };
 
-export default PrivateRoute;
+export default AdminRoute;
