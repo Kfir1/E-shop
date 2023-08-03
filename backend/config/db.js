b5565423c4aca982c65202f903a5000ec78035de
Kfir1 <kfirrp@gmail.com>
2023-08-03 13:53:05 +0300

    fix MONGO_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
     // calling mongoose.connect, will await and return a promise
     // accessing MONGO_URL from enviroment variable
-    const conn = await mongoose.connect(process.env.MONGO_URL)
+    const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`mongoDB connected: ${conn.connection.host}`)
   } catch (error) {
     console.log(`error: ${error.message}`)
