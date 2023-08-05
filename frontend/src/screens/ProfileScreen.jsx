1c705591a689164a05c477470a95ae811e072d6f
Kfir1 <kfirrp@gmail.com>
2023-08-05 16:34:51 +0300

    carousel buttons style + details button style on profileScreen

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -148,7 +148,7 @@ const ProfileScreen = () => {
                   </td>
                   <td>
                     <LinkContainer to={ `/order/${order._id}` }>
-                      <Button className="btn-sm" variant="light">
+                      <Button className="btn-sm" variant="primary">
                         Details
                       </Button>
                     </LinkContainer>
